9941fb8c9b86b7ba5725697fbd65617b4facf5c2
kaneko1102 <kaneko1102t.k@gmail.com>
2020-10-29 19:55:48 +0900

    修正

--- mail/send_mail.js
+++ mail/send_mail.js
@@ -1,11 +1,13 @@
-const sendmail = require('sendmail')();
-
-sendmail({
-    from: '',
-    to: '',
-    subject: 'テストメール',
-    text: 'メールの本文です。',
-  }, function(err, reply) {
-    console.log(err && err.stack);
-    console.dir(reply);
+const send = require('gmail-send')({
+  user: '',
+  pass: '',
+  to:   '',
+  subject: 'テスト',
 });
+
+send({
+  text:    'テスト \n テストメール\n',  
+}, (error, result, fullResult) => {
+  if (error) console.error(error);
+  console.log(result);
+})
