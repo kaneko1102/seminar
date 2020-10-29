ae01b25896c7e60a7ad339c16390a407b3b059ff
kaneko1102 <kaneko1102t.k@gmail.com>
2020-10-29 19:57:47 +0900

    first commit

---
+++ weather/get_weather.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+var URL = 'https://rss-weather.yahoo.co.jp/rss/days/4410.xml';
+
+var re_item = RegExp("(?<=<item>).*?(?=</item>)")
+var re_title = RegExp("(?<=<title>).*?(?=</title>)")
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var item = re_item.exec(body);
+    if(!item){
+      console.log("No title")
+    }else{
+	  var weather = re_title.exec(item[0]);
+	  console.log(weather[0]);
+	  
+    }
+  }else{
+    console.log("Error!");
+  }
+}
+
+request(URL, callback);
