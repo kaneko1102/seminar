e9d2d568cabff188b4fb52f0c4fef721981abc5d
kaneko1102 <kaneko-takuya@ed.tmu.ac.jp>
2020-10-30 12:05:04 +0900

    メール送信の関数に変更

--- mail/send_mail.js
+++ mail/send_mail.js
@@ -1,13 +1,20 @@
+
+function send_mail(user,pass,to,subject,text){
 const send = require('gmail-send')({
-  user: process.argv[3],
-  pass: process.argv[4],
-  to:   process.argv[2],
-  subject: 'テスト',
+  user: user,
+  pass: pass,
+  to:   to,
+  subject: subject,
 });
 
 send({
-  text:    'テスト \n テストメール\n',  
+  text:    text,  
 }, (error, result, fullResult) => {
   if (error) console.error(error);
   console.log(result);
 })
+}
+
+let subject = 'テスト'
+let text='テストメール\nこれはテストです。'
+send_mail(process.argv[3],process.argv[4],process.argv[2],subject,text);
